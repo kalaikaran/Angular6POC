e106b81a572bf17f28e0b0370bd6860e44056432
kalaikaran k <kalaikaran@yazhsoft.com>
2019-04-16 02:18:04 +0530

    testing

--- cust-prime-ng-app/src/app/app.module.ts
+++ cust-prime-ng-app/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompOneComponent } from './comp-one/comp-one.component';
 import { CardModule } from 'primeng/card';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+//import {ConfirmationService} from 'primeng/api';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,8 @@ import { CardModule } from 'primeng/card';
   imports: [
     BrowserModule,
     CardModule,
+    ConfirmDialogModule,
+    TooltipModule,
     AppRoutingModule
   ],
   providers: [],
